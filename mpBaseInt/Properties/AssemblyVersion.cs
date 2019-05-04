0e4e9dca7d902bd9329c4722abfe1208b173f9bd
Modis <cjmodis@gmail.com>
2019-05-04 09:33:33 +0300

    [2.2.0.8] Changes in STB 1704

--- mpBaseInt/Properties/AssemblyVersion.cs
+++ mpBaseInt/Properties/AssemblyVersion.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.2.0.7")]
-[assembly: AssemblyFileVersion("2.2.0.7")]
+[assembly: AssemblyVersion("2.2.0.8")]
+[assembly: AssemblyFileVersion("2.2.0.8")]

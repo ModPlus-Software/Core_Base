02f8ccdd9a505d05aa88dc506d02fbb14131262b
Modis <cjmodis@gmail.com>
2019-10-13 00:11:24 +0300

    [2.2.0.10] Добавлен диаметр 6.6 в ГОСТ 34028-2016

--- mpBaseInt/Properties/AssemblyVersion.cs
+++ mpBaseInt/Properties/AssemblyVersion.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.2.0.9")]
-[assembly: AssemblyFileVersion("2.2.0.9")]
+[assembly: AssemblyVersion("2.2.0.10")]
+[assembly: AssemblyFileVersion("2.2.0.10")]
